44e581b278411bb7c5e79b1c1c2270ac74bfee90
huangxingke <huangxingke03@163.com>
2023-07-25 09:15:01 +0800

    mod: 整理提交

--- lib/main.dart
+++ lib/main.dart
@@ -9,81 +9,95 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _battery_value = "0";
-  static const _CHANEL_NAME = "com.example.flutter_work/getBatteryLevel";
-  static const methodChannel = MethodChannel(_CHANEL_NAME);
-//获取原生电量
-  _getBattery() async {
-    String batteryLevel;
-    try {
-      final int result = await methodChannel.invokeMethod('getBatteryLevel');
-      batteryLevel = 'Battery level at $result % .';
-    } on PlatformException catch (e) {
-      batteryLevel = "Failed to get battery level: '${e.message}'.";
-    }
-    setState(() {
-      _battery_value = batteryLevel;
-    });
-  }
-
-//计数器
-  void _incrementCounter() {
-    setState(() {
-      _counter += 2;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '从安卓原生获取电量$_battery_value',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Container(
+      decoration: const BoxDecoration(color: Colors.white),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getBattery,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     colorScheme: ColorScheme.fromSeed(
+    //         seedColor: const Color.fromARGB(255, 183, 58, 71)),
+    //     useMaterial3: true,
+    //   ),
+    //   // home: const MyHomePage(title: ''),
+    // );
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//   String _battery_value = "0";
+//   static const _CHANEL_NAME = "com.example.flutter_work/getBatteryLevel";
+//   static const methodChannel = MethodChannel(_CHANEL_NAME);
+// //获取原生电量
+//   _getBattery() async {
+//     String batteryLevel;
+//     try {
+//       final int result = await methodChannel.invokeMethod('getBatteryLevel');
+//       batteryLevel = 'Battery level at $result % .';
+//     } on PlatformException catch (e) {
+//       batteryLevel = "Failed to get battery level: '${e.message}'.";
+//     }
+//     setState(() {
+//       _battery_value = batteryLevel;
+//     });
+//   }
+
+// //计数器
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter += 2;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '从安卓原生获取电量$_battery_value',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _getBattery,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
